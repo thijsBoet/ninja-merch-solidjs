0fd1b4821b302f5c844b4f34c11d6a54705ce515
Matthijs Boet <m.boet@chello.nl>
2023-05-03 14:40:03 +0200

    fetchAPI for ProductId not working

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,34 @@
 import { useParams } from "@solidjs/router";
+import { Show, createResource } from "solid-js";
+
+const fetchProduct = async (id) => {
+	const res = await fetch('http://localhost:8080/products/' + id);
+	return await res.json();
+};
 
 export default function Product() {
     const params = useParams();
-    
+
+	const [product] = createResource(params.id, fetchProduct);
 
     return (
-		<>
-			<div>Product Details - {params.id}</div>
-			<p>{console.log(params)}</p>
-		</>
+		<div class='my-7'>
+			<Show when={product()} fallback={<p>Loading product...</p>}>
+				<div class='grid grid-cols-5 gap-7'>
+
+					<div class='col-span-2'>
+						<img src={product().img} alt='product image' />
+					</div>
+
+					<div class='col-span-3'>
+						<h2 class='text-3xl font-bold mb-7'>
+							{product().title}
+						</h2>
+						<p>{product().description}</p>
+						<p class='my-7 text-2xl'>Only ${product().price}</p>
+					</div>
+				</div>
+			</Show>
+		</div>
 	);
 }
